e2436c7c6463b9fe96fe4e9e911cbd8cff485836
Аккожаев Жасулан Ерболатулы <@lcoast.itstep>
2022-06-10 21:45:39 +0600

    server json request

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+
 const express = require('express')
 
 const app = new express()
+
+app.use(express.json())
+
+app.post('/user', (request, response) => {
+    console.log(request.body.userNumber)
+})
 
 app.use(express.static('static')).listen(9999, () => {
     console.log('Express server listens on port 9999')
--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,20 @@
 const playBtn = document.querySelector('.btn')
 const inp = document.querySelector('.input')
+const userNumber = document.querySelector('.userNumber')
 
 playBtn.addEventListener('click', () => {
-    alert(inp.value)
+    if(inp.value !== '' && inp.value <= 5) {
+        alert(inp.value)
+    } else {
+        alert('Choose a number between 1 and 5')
+    }
+
+    const obj = {userNumber: userNumber.value}
+
+    fetch('/user', {
+        method: 'POST',
+        body: JSON.stringify(obj),
+        headers: { 'Content-Type': 'application/json'}
+    })
 })
 
